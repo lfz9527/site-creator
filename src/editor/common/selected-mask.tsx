d120d308a39bbb18938409465974220524e56935
lifangzheng <1397118980@qq.com>
2024-11-11 11:16:34 +0800

    feat: 修改类型不正确

--- src/editor/common/selected-mask.tsx
+++ src/editor/common/selected-mask.tsx
@@ -1,182 +1,190 @@
 import {
-  forwardRef,
-  useEffect,
-  useImperativeHandle,
-  useRef,
-  useState,
-  useMemo,
-} from "react";
-import { createPortal } from "react-dom";
-import { DeleteOutlined } from "@ant-design/icons";
-import { observeContainer } from "@editor/utils";
-import { useComponents, useComponentConfigStore } from "@editor/stores";
-import { getComponentById } from "@editor/utils";
+    forwardRef,
+    useEffect,
+    useImperativeHandle,
+    useRef,
+    useState,
+    useMemo
+} from 'react'
+import {createPortal} from 'react-dom'
+import {DeleteOutlined} from '@ant-design/icons'
+import {observeContainer} from '@editor/utils'
+import {useComponents, useComponentConfigStore} from '@editor/stores'
+import {getComponentById} from '@editor/utils'
 
 interface Props {
-  // 组件id
-  componentId: string;
-  // 容器class
-  containerClassName: string;
-  // 相对容器class
-  offsetContainerIdName: string;
+    // 组件id
+    componentId: string
+    // 容器class
+    containerClassName: string
+    // 相对容器class
+    offsetContainerIdName: string
 }
 
 function SelectedMask(
-  { componentId, containerClassName, offsetContainerIdName }: Props,
-  ref: any
+    {componentId, containerClassName, offsetContainerIdName}: Props,
+    ref: any
 ) {
-  const toolRef = useRef<HTMLElement>();
-  const [position, setPosition] = useState({
-    left: 0,
-    top: 0,
-    width: 0,
-    height: 0,
-    toolsTop: 0,
-    toolsLeft: 0,
-    rootToolsTop: 0,
-    rootToolsLeft: 0,
-  });
-  const { componentConfig } = useComponentConfigStore();
-  const { components, deleteComponent, setCurComponentId } = useComponents();
-  const curComponent = useMemo(() => {
-    return getComponentById(componentId, components);
-  }, [componentId]);
+    const toolRef = useRef<HTMLDivElement>(null)
+    const [position, setPosition] = useState({
+        left: 0,
+        top: 0,
+        width: 0,
+        height: 0,
+        toolsTop: 0,
+        toolsLeft: 0,
+        rootToolsTop: 0,
+        rootToolsLeft: 0
+    })
+    const {componentConfig} = useComponentConfigStore()
+    const {components, deleteComponent, setCurComponentId} = useComponents()
+    const curComponent = useMemo(() => {
+        return getComponentById(componentId, components)
+    }, [componentId])
 
-  const curComponentConfig = componentConfig[curComponent?.name || ""];
-  const { isRoot, desc } = curComponentConfig;
+    const curComponentConfig = componentConfig[curComponent?.name || '']
+    const {isRoot, desc} = curComponentConfig
 
-  // 对外暴露更新位置方法
-  useImperativeHandle(ref, () => ({
-    updatePosition,
-  }));
+    // 对外暴露更新位置方法
+    useImperativeHandle(ref, () => ({
+        updatePosition
+    }))
 
-  useEffect(() => {
-    const { resizeObserver, container } = observeContainer(
-      () => {
-        updatePosition();
-      },
-      { dataComponentId: componentId }
-    );
-    return () => {
-      if (container) {
-        resizeObserver.unobserve(container);
-      }
-    };
-  }, [componentId]);
+    useEffect(() => {
+        const {resizeObserver, container} = observeContainer(
+            () => {
+                updatePosition()
+            },
+            {dataComponentId: componentId}
+        )
+        return () => {
+            if (container) {
+                resizeObserver.unobserve(container)
+            }
+        }
+    }, [componentId])
 
-  useEffect(() => {
-    updatePosition();
-  }, [componentId]);
+    useEffect(() => {
+        updatePosition()
+    }, [componentId])
 
-  function updatePosition() {
-    if (!componentId) return;
+    function updatePosition() {
+        if (!componentId) return
 
-    const container = document.querySelector(`#${offsetContainerIdName}`);
+        const container = document.querySelector(`#${offsetContainerIdName}`)
 
-    if (!container) return;
+        if (!container) return
 
-    const node = document.querySelector(`[data-component-id="${componentId}"]`);
+        const node = document.querySelector(
+            `[data-component-id="${componentId}"]`
+        )
 
-    if (!node) return;
+        if (!node) return
 
-    // 获取节点位置
-    const { top, left, width, height } = node.getBoundingClientRect();
-    // 获取容器位置
-    const { top: containerTop, left: containerLeft } =
-      container.getBoundingClientRect();
+        // 获取节点位置
+        const {top, left, width, height} = node.getBoundingClientRect()
+        // 获取容器位置
+        const {top: containerTop, left: containerLeft} =
+            container.getBoundingClientRect()
 
-    const toolHeight = toolRef?.current?.clientHeight || 20;
-    let toolsLeft = left - containerLeft + width;
+        const toolHeight = toolRef?.current?.clientHeight || 20
+        let toolsLeft = left - containerLeft + width
 
-    // 工具top的位置
-    let toolsTop = top - containerTop + container.scrollTop;
+        // 工具top的位置
+        let toolsTop = top - containerTop + container.scrollTop
 
-    // 如果工具组件的高度超过了容器的高度，那么就将工具组件的位置向下移动到组件的下方
-    if (toolsTop < toolHeight) {
-      toolsTop = toolsTop + height + 20;
+        // 如果工具组件的高度超过了容器的高度，那么就将工具组件的位置向下移动到组件的下方
+        if (toolsTop < toolHeight) {
+            toolsTop = toolsTop + height + 20
+        }
+        if (toolsTop <= 0) {
+            toolsTop -= -30
+            toolsLeft -= 10
+        }
+
+        // 计算位置
+        setPosition({
+            top: top - containerTop + container.scrollTop,
+            left: left - containerLeft,
+            width,
+            height,
+            toolsTop: toolsTop,
+            toolsLeft: toolsLeft,
+            rootToolsTop: top - containerTop + container.scrollTop + 32,
+            rootToolsLeft: width - 16
+        })
     }
-    if (toolsTop <= 0) {
-      toolsTop -= -30;
-      toolsLeft -= 10;
+
+    const deleteHandle = () => {
+        deleteComponent(componentId)
+        setCurComponentId(null)
     }
 
-    // 计算位置
-    setPosition({
-      top: top - containerTop + container.scrollTop,
-      left: left - containerLeft,
-      width,
-      height,
-      toolsTop: toolsTop,
-      toolsLeft: toolsLeft,
-      rootToolsTop: top - containerTop + container.scrollTop + 32,
-      rootToolsLeft: width - 16,
-    });
-  }
+    const maskContainer = document.querySelector(`.${containerClassName}`)
 
-  const deleteHandle = () => {
-    deleteComponent(componentId);
-    setCurComponentId(null);
-  };
-
-  const maskContainer = document.querySelector(`.${containerClassName}`);
-
-  // @ts-ignore
-  function MaskTag({ children }) {
-    return (
-      <div className="flex items-center justify-center px-[4px] h-[20px] bg-[var(--edit-primary-color)] whitespace-nowrap">
-        {children}
-      </div>
-    );
-  }
-
-  if (maskContainer) {
-    return createPortal(
-      <>
-        <div
-          className="absolute border-x-2 border-y-2 border-solid border-[var(--edit-primary-color)] pointer-events-none z-[1003] rounded-[4px] box-border"
-          style={{
-            left: position.left,
-            top: position.top,
-            width: position.width,
-            height: position.height,
-          }}
-        />
-        {isRoot && (
-          <div
-            className="absolute text-white text-[12px] z-11 -translate-x-full -translate-y-full"
-            style={{
-              left: position.rootToolsLeft,
-              top: position.rootToolsTop,
-            }}
-          >
-            <MaskTag>{desc}</MaskTag>
-          </div>
-        )}
-        <div
-          ref={toolRef}
-          className="absolute text-[14px] z-11 -translate-y-full -translate-x-full"
-          style={{
-            left: position.toolsLeft,
-            top: position.toolsTop,
-            display: !position.width || position.width < 10 ? "none" : "inline",
-          }}
-        >
-          {!isRoot && (
-            <div className="flex items-center justify-center gap-[5px]">
-              <MaskTag>
-                <div className="cursor-pointer" onClick={deleteHandle}>
-                  <DeleteOutlined style={{ color: "#fff" }} />
-                </div>
-              </MaskTag>
+    // @ts-ignore
+    function MaskTag({children}) {
+        return (
+            <div className='flex items-center justify-center px-[4px] h-[20px] bg-[var(--edit-primary-color)] whitespace-nowrap'>
+                {children}
             </div>
-          )}
-        </div>
-      </>,
-      maskContainer!
-    );
-  }
+        )
+    }
 
-  return null;
+    if (maskContainer) {
+        return createPortal(
+            <>
+                <div
+                    className='absolute border-x-2 border-y-2 border-solid border-[var(--edit-primary-color)] pointer-events-none z-[1003] rounded-[4px] box-border'
+                    style={{
+                        left: position.left,
+                        top: position.top,
+                        width: position.width,
+                        height: position.height
+                    }}
+                />
+                {isRoot && (
+                    <div
+                        className='absolute text-white text-[12px] z-11 -translate-x-full -translate-y-full'
+                        style={{
+                            left: position.rootToolsLeft,
+                            top: position.rootToolsTop
+                        }}
+                    >
+                        <MaskTag>{desc}</MaskTag>
+                    </div>
+                )}
+                <div
+                    ref={toolRef}
+                    className='absolute text-[14px] z-11 -translate-y-full -translate-x-full'
+                    style={{
+                        left: position.toolsLeft,
+                        top: position.toolsTop,
+                        display:
+                            !position.width || position.width < 10
+                                ? 'none'
+                                : 'inline'
+                    }}
+                >
+                    {!isRoot && (
+                        <div className='flex items-center justify-center gap-[5px]'>
+                            <MaskTag>
+                                <div
+                                    className='cursor-pointer'
+                                    onClick={deleteHandle}
+                                >
+                                    <DeleteOutlined style={{color: '#fff'}} />
+                                </div>
+                            </MaskTag>
+                        </div>
+                    )}
+                </div>
+            </>,
+            maskContainer!
+        )
+    }
+
+    return null
 }
 
-export default forwardRef(SelectedMask);
+export default forwardRef(SelectedMask)
